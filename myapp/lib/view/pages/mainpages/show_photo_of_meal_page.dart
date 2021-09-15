3818d09f5845b30d33c5ded2edf053c49a52a71c
Yunho <potatohead12@naver.com>
2021-09-15 23:54:17 +0900

    9월 15일 부대 급식 자랑 수정

--- myapp/lib/view/pages/mainpages/show_photo_of_meal_page.dart
+++ myapp/lib/view/pages/mainpages/show_photo_of_meal_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -9,29 +10,13 @@ class ShowPhotoOfMealPage extends StatelessWidget {
       child: Column(
         children: [
           Container(
-            child: Row(
-              children: [
-                Icon(Icons.whatshot),
-                SizedBox(width: 2),
-                Text(
-                  "우리 부대 급식 자랑",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Spacer(),
-                TextButton(
-                  onPressed: () {},
-                  child: Text("게시하기"),
-                ),
-              ],
-            ),
+            child: _buildHeader(),
           ),
           Expanded(
             child: GridView.builder(
               scrollDirection: Axis.vertical,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 10 / 10,
                 crossAxisCount: 2,
               ),
               itemBuilder: (context, index) {
@@ -45,24 +30,114 @@ class ShowPhotoOfMealPage extends StatelessWidget {
     );
   }
 
+  Widget _buildHeader() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Row(
+        children: [
+          Icon(Icons.whatshot),
+          SizedBox(width: 2),
+          Text(
+            "우리 부대 급식 자랑",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Spacer(),
+          TextButton(
+            onPressed: () {},
+            child: Text("게시하기"),
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget _buildGridViewItem(BuildContext context, int index) {
-    double _imageWidth = MediaQuery.of(context).size.width * 0.5 - 4;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    double _imageWidth = MediaQuery.of(context).size.width * 0.5;
+    String image = "meal_photos/example_${index + 1}.jpg";
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: InkWell(
+        onTap: () {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) {
+              return _simpleDialog(context, image, _imageWidth);
+            },
+          );
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              image,
+              fit: BoxFit.cover,
+              width: _imageWidth,
+              height: _imageWidth * 0.6,
+            ),
+            Container(
+              padding: EdgeInsets.all(3),
+              alignment: Alignment.topLeft,
+              width: _imageWidth,
+              height: _imageWidth * 0.3,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "제8전투비행단",
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  Text(
+                    "2021년 9월 8일 (석식)",
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SimpleDialog _simpleDialog(
+      BuildContext context, String image, double _imageWidth) {
+    return SimpleDialog(
+      contentPadding: const EdgeInsets.all(16),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "8전투비행단",
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "2021년 9월 8일 (석식)",
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+      titlePadding: const EdgeInsets.all(16),
       children: [
         Image.asset(
-          "meal_photos/example_${index + 1}.jpg",
-          fit: BoxFit.cover,
-          width: _imageWidth,
-          height: _imageWidth * 0.6,
+          image,
+          fit: BoxFit.contain,
+          // width: _imageWidth * 1.5,
         ),
-        Text(
-          "제8전투비행단",
-          style: TextStyle(fontSize: 10),
-        ),
-        Text(
-          "2021년 9월 8일 (석식)",
-          style: TextStyle(fontSize: 10),
+        Divider(),
+        Row(
+          children: [
+            Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("돌아가기"),
+            ),
+          ],
         ),
       ],
     );
