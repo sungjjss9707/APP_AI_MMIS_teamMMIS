a1bc1d4ddf9903a7bdd8358c3fb014ab705eff7f
Minho Choi <36256238+Paspachu@users.noreply.github.com>
2021-09-18 10:25:48 +0000

    초기 데이터 및 전처리 방식 추가

--- AI/DataLoadTest.py
+++ AI/DataLoadTest.py
@@ -5,7 +5,8 @@ import numpy as np
 
 # %%
 # 데이터를 읽어옵니다.
-data = pd.read_csv("제7652부대 식단 정보_월별.csv", encoding="CP949")
+unitNum = 7652
+data = pd.read_csv("Raw CSV Data Files/제" + str(unitNum) + "부대 식단 정보_월별.csv", encoding="CP949")
 data
 
 #%%
@@ -15,9 +16,12 @@ with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 
 # %%
 # 데이터에 값이 아예 없는 열은 제외합니다.
-for column in data:
-    if data[column].count() == 0:
-        data = data.drop(column, axis=1)
+data = data.dropna(axis=1, how='all')
+data
+
+#%%
+# 증특식 열의 필요성을 못 느껴 제외합니다.
+data = data.drop('증특식', axis=1)
 data
 
 # %%
@@ -30,16 +34,21 @@ data
 
 # %%
 # 날짜가 비어있는 열은 제외합니다.
-for index, row in data.iterrows():
-    if row['날짜'] is np.NAN:
-        data = data.drop([index], axis=0)
+data = data[data['날짜'].notna()]
+data = data.reset_index(drop=True)
 data
 
 # %%
-
+# 메뉴이름, 식사명, 열량, 날짜, 부대의 형태의 테이블로 변경합니다.
+processed_data = pd.DataFrame(columns=['메뉴이름', '식사명', '열량', '날짜', '부대'])
+for index, row in data.iterrows():
+    processed_data.loc[index * 3] = [row['조식'], '조식', row['조식열량'], row['날짜'], unitNum]
+    processed_data.loc[index * 3 + 1] = [row['중식'], '중식', row['중식열량'], row['날짜'], unitNum]
+    processed_data.loc[index * 3 + 2] = [row['석식'], '석식', row['석식열량'], row['날짜'], unitNum]
+processed_data
 
 # %%
-
-
-#%%
-
+# 메뉴이름이 없는 열은 제외합니다.
+processed_data = processed_data[processed_data['메뉴이름'].notna()]
+processed_data = processed_data.reset_index(drop=True)
+processed_data
