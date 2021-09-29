968eec5397c13f67e706dae009e64fab1e8ea805
Ubuntu <ubuntu@ip-172-31-34-148.ap-northeast-2.compute.internal>
2021-09-29 13:52:09 +0000

    get_api 210929

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-app.set('port', process.env.PORT || 8080);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,7 +11,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.set('port', process.env.PORT || 8080);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
