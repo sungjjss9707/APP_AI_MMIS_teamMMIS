3f6145305dacf3f4cddb09df21b9f47d1504fa55
mini-777 <pho0570@gmail.com>
2021-10-03 05:46:56 +0000

    update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res) => {
+  console.log(req.params, req.query);
+    
+    connection.query(`select * from board`, (error, results, fields) => {
+
+        if (error) {
+            console.log(error);
+        }else{
+            // res.send({result: 'success'});
+            console.log(results);
+        }
+    })
 });
 
 module.exports = router;
