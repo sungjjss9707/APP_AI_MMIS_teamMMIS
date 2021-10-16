c3aa763f30a47401e72750400e2d67360e512e2a
onlyshine <76477530+onlyshine@users.noreply.github.com>
2021-10-16 02:03:38 +0000

    clustering model tuning 완료, 서벼 완료

--- AI/n-gram clustering.py
+++ AI/n-gram clustering.py
@@ -10,7 +10,6 @@ from pandas.core.frame import  DataFrame
 from konlpy.tag import Okt 
 from konlpy.tag import Komoran
 
-# %%
 #데이터 불러오기
 data= pd.read_csv("All Menu (Various Versions)/국방부메뉴_v2.1.csv", encoding="UTF-8")
 data
@@ -26,14 +25,12 @@ data = data.drop_duplicates(['메뉴이름'], ignore_index=True)
 
 for i in data:
     if(i in ['계란류', '우유', '메밀', '땅콩', '대두', '밀', '고등어', '게', '새우', '돼지고기', '복숭아', '토마토', '아황산류', '호두', '닭고기', '쇠고기', '오징어', '조개류', '잣']):
-        data[i] = data[i]/500
+        data[i] = data[i]/100
     if i in ['열량', '탄수화물', '지방', '단백질', '나트륨', '콜레스트롤']:
         data[i] = ((data[i]-data[i].mean())/data[i].std())/500
-data
 
 
 
-#%%
 # bigram/ trigram 만들기
 bigram=[]
 trigram =[]
@@ -60,14 +57,12 @@ for menuName in data['메뉴이름']:
             temp.append(str(menuName[i]+menuName[i+1]+menuName[i+2]))    
         trigram.append(temp)
 
-# %%
 # data축에 삽입
 data.insert(2, 'bigram',bigram)
 
 data.insert(3, 'trigram',trigram)
 
-data
-# %%
+
 # tf-idf 적용
  
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -91,19 +86,25 @@ bigram_vector = bigram_tfidf_vectorizer.transform(bigramText).toarray()
 trigram_vector=trigram_tfidf_vectorizer.transform(trigramText).toarray()
 
 
-# %%
 # k-means clustering
 
-from sklearn.cluster import KMeans
+
 
 bigram_dataList=[]
-for vectorList in bigram_vector:
-    bigram_dataList.append(vectorList.tolist())
+for i in range(0, 1550):
+    bigram_vector_data = bigram_vector[i].tolist()
+    for j in ['계란류', '우유', '메밀', '땅콩', '대두', '밀', '고등어', '게', '새우', '돼지고기', '복숭아', '토마토', '아황산류', '호두', '닭고기', '쇠고기', '오징어', '조개류', '잣', '열량', '탄수화물', '지방', '단백질', '나트륨', '콜레스트롤']:
+        bigram_vector_data.append(data[j][i])
+    bigram_dataList.append(bigram_vector_data)
 
 trigram_dataList=[]
-for vectorList in trigram_vector:
-    trigram_dataList.append(vectorList.tolist())
+for i in range(0, 1550):
+    trigram_vector_data = trigram_vector[i].tolist()
+    for j in ['계란류', '우유', '메밀', '땅콩', '대두', '밀', '고등어', '게', '새우', '돼지고기', '복숭아', '토마토', '아황산류', '호두', '닭고기', '쇠고기', '오징어', '조개류', '잣', '열량', '탄수화물', '지방', '단백질', '나트륨', '콜레스트롤']:
+        trigram_vector_data.append(data[j][i])
+    trigram_dataList.append(bigram_vector_data)
 
+from sklearn.cluster import KMeans
 num_clusters = 10
 
 k_means_clustering = KMeans(n_clusters=num_clusters)
@@ -114,8 +115,7 @@ trigram_idx = k_means_clustering.fit_predict(trigram_dataList)
 data['category_bigram']=bigram_idx
 data['category_trigram']=trigram_idx
 
-
-#%%
+"""
 # list로 cluster 결과 확인
 clusterNum = 0
 clustred_data=pd.DataFrame()
@@ -125,19 +125,18 @@ clustred_data.insert(0, 'category_trigram', data['category_trigram'])
 clustred_data
 
 with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print(clustred_data.loc[clustred_data['category_bigram']==clusterNum])
+    print(clustred_data.loc[clustred_data['category_bigram']==clusterNum])"""
 
 
-# %%
 #Embedding & 시각화
 from sklearn.manifold import TSNE
 import os.path
 import pickle
 
-#%%
 #차원축소
-X = trigram_vector.tolist()
+X = bigram_vector.tolist()
 y = data['category_bigram'].tolist()
+
 
 """tsne_filepath = 'tnse3000(w2v).pkl'
 
@@ -158,14 +157,13 @@ tsne_df = pd.DataFrame(tsne_points, index=range(len(X)), columns=['x_coord', 'y_
 tsne_df['menu_name']=data['메뉴이름'].tolist()
 tsne_df['cluster_number'] = y
 
-# %%
 #2차원 plotting
 from bokeh.plotting import figure, show, output_notebook
 from bokeh.models import HoverTool, ColumnDataSource, value
 from bokeh.palettes import brewer
 
 output_notebook()
-# %%
+
 # Get the number of colors we'll need for the plot.
 colors = brewer["Spectral"][len(tsne_df['cluster_number'].unique())]
 
@@ -176,7 +174,7 @@ colormap = {i: colors[i] for i in tsne_df['cluster_number'].unique()}
 colors = [colormap[x] for x in tsne_df['cluster_number']]
 
 tsne_df['color']=colors
-# %%
+
 # Bokeh Datasouce 만들기
 plot_data = ColumnDataSource(
     data=tsne_df.to_dict(orient='list')
@@ -219,27 +217,25 @@ tsne_plot.outline_line_color = None
 # 짠!
 show(tsne_plot)
 
-# %%
 # 유사 메뉴 추천 
 
 from sklearn.metrics.pairwise import cosine_similarity
 import warnings; warnings.filterwarnings('ignore')
 
-#%%
 # 코사인 유사도 계산
-menuNameSimilarity = cosine_similarity(bigram_dataList, bigram_dataList)
-
+bigram_menuNameSimilarity = cosine_similarity(bigram_dataList, bigram_dataList)
+trigram_menuNameSimilarity = cosine_similarity(trigram_dataList, trigram_dataList)
 # 유사도 정렬
-menu_sim_sorted_idx = menuNameSimilarity.argsort()[:, ::-1]
+bigram_menu_sim_sorted_idx = bigram_menuNameSimilarity.argsort()[:, ::-1]
+trigram_menu_sim_sorted_idx = trigram_menuNameSimilarity.argsort()[:, ::-1]
 
-#%%
 # 유사 메뉴 추천 함수
 def find_sim_menu(data, sorted_idx, name, number=10):
     title_menu=data[data['메뉴이름']==name]
 
     title_menu_idx = title_menu.index.values
 
-    top_sim_idx = menu_sim_sorted_idx[title_menu_idx, :number]
+    top_sim_idx = sorted_idx[title_menu_idx, :number]
     
     top_sim_idx = top_sim_idx.reshape(-1,)
     similar_menu = data.iloc[top_sim_idx]['메뉴이름']
@@ -249,19 +245,19 @@ def find_sim_menu(data, sorted_idx, name, number=10):
         similar_menu_list.append(sim_menu)
 
 
-    return similar_menu_list
+    return similar_menu_list[1:4]
+
+
+# 메뉴 추천 Test
+recommendMenus = ['청양마요치킨','비엔나소시지찌개','햄치즈버거','탕수육', '두부고추장찌개','낙지덮밥','꼬리곰탕','김장김치','콘형아이스크림']
+print("Bigram")
+for menu in recommendMenus:
+    print(menu, end=' : ')
+    print(find_sim_menu(data, bigram_menu_sim_sorted_idx, menu))
 
 
 
 #%%
-# 메뉴 추천
-recommendMenu = '부추비빔밥'
-
-similar_menus=find_sim_menu(data, menu_sim_sorted_idx, recommendMenu)
-
-
-similar_menus
-
-#%%
-np.save('/workspaces/APP_AI_MMIS_teamMMIS/AI/server/AI file/menu_sim_sorted_idx',menu_sim_sorted_idx)
-data.to_csv('/workspaces/APP_AI_MMIS_teamMMIS/AI/server/AI file/data.csv')
+np.save('/workspaces/APP_AI_MMIS_teamMMIS/AI/server/AI file/bigram_menu_sim_sorted_idx',bigram_menu_sim_sorted_idx)
+"""data.to_csv('/workspaces/APP_AI_MMIS_teamMMIS/AI/server/AI file/data.csv')"""
+# %%
--- AI/server/server.py
+++ AI/server/server.py
@@ -19,8 +19,8 @@ CORS(app)
 
 # 필요 data 읽기
 data = pd.read_csv("/workspaces/APP_AI_MMIS_teamMMIS/AI/server/AI file/data.csv", encoding="UTF-8")
-menu_sim_sorted_idx=np.load("/workspaces/APP_AI_MMIS_teamMMIS/AI/server/AI file/menu_sim_sorted_idx.npy")
-
+w2v_menu_sim_sorted_idx=np.load("/workspaces/APP_AI_MMIS_teamMMIS/AI/server/AI file/w2v_menu_sim_sorted_idx.npy")
+bigram_menu_sim_sorted_idx=np.load("/workspaces/APP_AI_MMIS_teamMMIS/AI/server/AI file/bigram_menu_sim_sorted_idx.npy")
 
 # Flutter 앱에서 이 친구를 호출할 예정 (body 데이터를 가져오려면 http 규칙이 post로 전송)
 
@@ -50,7 +50,18 @@ def main():
     menuRecommend=[]
 
     for menu in cmReqDto["menus"]:
-        menuList.append(p.find_sim_menu(data, menu_sim_sorted_idx, menu))
+        bigram_w2v_menus=[]
+        bigramTemp=p.find_sim_menu(data, bigram_menu_sim_sorted_idx, menu)
+        w2vTemp=p.find_sim_menu(data, w2v_menu_sim_sorted_idx,menu)
+        for temp in w2vTemp:
+            bigram_w2v_menus.append(temp)
+        for temp in bigramTemp:
+            if not temp in bigram_w2v_menus:
+                bigram_w2v_menus.append(temp)
+
+        menuList.append(bigram_w2v_menus[:5])
+    
+    print(menuList)
 
     if len(cmReqDto["menus"])==3:
         for j in product(menuList[0],menuList[1],menuList[2]):
@@ -95,4 +106,4 @@ def main():
     return jsonify(cmRespDto)
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0', debug=True, port=5500)
+    app.run(host = '0.0.0.0', port=5500)
